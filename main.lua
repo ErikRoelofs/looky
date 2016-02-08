8c97cbfe6c126f594046c15fe8cb2bd643b9b1f2
Erik Roelofs <eroelofs@zigwebsoftware.nl>
2016-02-08 17:53:42 +0100

    layout builder

--- main.lua
+++ main.lua
@@ -2,20 +2,20 @@ function love.load()
   --mobdebug = require("mobdebug") mobdebug.start()
   root = newRoot()
   
-  layout = newLayout(150,"fill",{100,200,50,100})
+  layout = buildLayout("linear", {width = 150, height = "fill", color = {100,200,50,100}, marginRight = 20, marginLeft = 20})
   root:addChild(layout)
   
-  layout2 = newLayout("fill","fill",{255,0,0,200})
+  layout2 = buildLayout("text", {width = 150, height = "fill", text = "this is a text item", textColor = {100,200,50,100}, paddingLeft = 10, paddingTop = 10})
   root:addChild(layout2)
   
-  layout3 = newLayout(250,"fill",{0,255,0,200})
+  layout3 = buildLayout("linear", {width = 250, height = "fill", color = {0, 255, 0, 200}})
   root:addChild(layout3)
 
-  layout4 = newLayout("fill","fill",{255,0,0,200})
+  layout4 = buildLayout("linear", {width = "fill", height = "fill", color = {255, 255, 0, 200}, paddingLeft = 50, paddingBottom = 200, paddingTop = 25})
   root:addChild(layout4)
 
-  layout:addChild( newLayout( "fill", 120, {200, 50, 50, 200}))
-  layout:addChild( newLayout( "fill", 40, {20, 30, 40, 200}))
+  --layout:addChild( newLayout( "fill", 120, {200, 50, 50, 200}))
+  --layout:addChild( newLayout( "fill", 40, {20, 30, 40, 200}))
   
   root:layoutingPass()
 end
@@ -77,6 +77,7 @@ function newLayout(width, height, color)
     givenHeight = 0,
     color = color,
     direction = "v",
+    text = "test",
     addChild = function(self, child)
       table.insert(self.children, child)
       child:setParent(self)
@@ -104,6 +105,8 @@ function newLayout(width, height, color)
       
     end,
     render = function(self)
+      renderText(self)
+      --[[
       love.graphics.setColor(color)
       love.graphics.rectangle("fill", 0, 0, self:grantedWidth(), self:grantedHeight())
       
@@ -122,7 +125,118 @@ function newLayout(width, height, color)
         v:render()
         
         love.graphics.pop()
+      end]]--
+    end
+  }
+end
+
+renderText = function(self)
+  love.graphics.setColor(self.color)
+  local width = self:grantedWidth() - (self.paddingLeft + self.paddingRight)
+  local height = self:grantedHeight() - (self.paddingTop + self.paddingBottom)
+  love.graphics.rectangle("fill", self.paddingLeft, self.paddingTop, width, height)
+  if self.text ~= "linear" then
+    love.graphics.setColor(self.textColor or {255,255,255,255})
+    love.graphics.print(self.text,self.paddingLeft,self.paddingTop)
+  end
+  
+  local offset = 0
+  
+  for k, v in ipairs(self.children) do
+    love.graphics.push()
+    if self.direction == "v" then 
+      love.graphics.translate(0, offset)
+      offset = offset + v:grantedHeight()
+    else
+      love.graphics.translate(offset, 0)
+      offset = offset + v:grantedWidth()
+    end
+      
+    v:render()
+    
+    love.graphics.pop()
+  end
+end
+
+function buildLayout(kind, options)
+  local start = baseLayout(options.width, options.height)
+  start.paddingLeft = options.paddingLeft or 0
+  start.paddingTop = options.paddingTop or 0
+  start.paddingRight = options.paddingRight or 0
+  start.paddingBottom = options.paddingBottom or 0
+  start.marginLeft = options.marginLeft or 0
+  start.marginTop = options.marginTop or 0
+  start.marginRight = options.marginRight or 0
+  start.marginBottom = options.marginBottom or 0
+  
+  if kind == "linear" then
+    start.render = renderText
+    start.layoutingPass = verticalLayout
+    start.color = options.color
+    start.text = "linear"
+    start.textColor = {255,255,255,255}
+  elseif kind == "text" then
+    start.render = renderText
+    start.layoutingPass = verticalLayout    
+    start.color = {0,0,0,255}
+    start.text = options.text
+    start.textColor = options.textColor or {255,255,255,255}
+  end
+  return start
+end
+
+function baseLayout(width, height)
+  return {
+    children = {},
+    parent = nil,
+    width = width,
+    height = height,
+    givenWidth = 0,
+    givenHeight = 0,
+    paddingLeft = 0,
+    paddingRight = 0,
+    paddingTop = 0,
+    paddingBottom = 0,
+    marginLeft = 0,
+    marginRight = 0,
+    marginTop = 0,
+    marginBottom = 0,
+    addChild = function(self, child)
+      table.insert(self.children, child)
+      child:setParent(self)
+    end,
+    setParent = function(self, parent)
+      self.parent = parent
+    end,
+    desiredWidth = function(self)      
+      if self.width == "fill" then
+        return self.width
+      else
+        return self.width + self.marginLeft + self.marginRight
       end
+    end,
+    desiredHeight = function(self)
+      if self.height == "fill" then
+        return self.height 
+      else
+        return self.height + self.marginTop + self.marginBottom
+      end
+    end,
+    grantedWidth = function(self)
+      return self.givenWidth
+    end,
+    grantedHeight = function(self)
+      return self.givenHeight
+    end,
+    setDimensions = function(self, x, y)
+      self.givenWidth = x
+      self.givenHeight = y
+    end,
+    layoutingPass = function(self)
+      
+    end,
+    render = function(self)
+    
     end
   }
 end
