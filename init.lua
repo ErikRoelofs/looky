9f48155c77296dae0984c35b10958e61bcd8cc70
Erik Roelofs <erik@ruigekonijnen.nl>
2016-06-15 07:50:31 +0200

    Fixed the init script

--- init.lua
+++ init.lua
@@ -1,13 +1,13 @@
 local _NAME = ...
   
 local lc = require ( _NAME .. "/layoutcreator" )
-lc:register("linear", require( _NAME .. "/layout/linear" ))
-lc:register("text", require( _NAME .. "/layout/text" ))
-lc:register("image", require( _NAME .. "/layout/image" ))
-lc:register("caption", require( _NAME .. "/layout/caption" ))
-lc:register("list", require( _NAME .. "/layout/list" ))
-lc:register("root", require( _NAME .. "/layout/root" ))
-lc:register("stack", require( _NAME .. "/layout/stack" ))
-lc:register("freeform", require( _NAME .. "/layout/freeform" ))
+lc:register("linear", require( _NAME .. "/layout/linear" )(lc))
+lc:register("text", require( _NAME .. "/layout/text" )(lc))
+lc:register("image", require( _NAME .. "/layout/image" )(lc))
+lc:register("caption", require( _NAME .. "/layout/caption" )(lc))
+lc:register("list", require( _NAME .. "/layout/list" )(lc))
+lc:register("root", require( _NAME .. "/layout/root" )(lc))
+lc:register("stack", require( _NAME .. "/layout/stack" )(lc))
+lc:register("freeform", require( _NAME .. "/layout/freeform" )(lc))
 
 return lc
