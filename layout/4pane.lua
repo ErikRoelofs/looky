1489fd6f987124354d58cf7544cf37c3069c8103
Erik Roelofs <eroelofs@zigwebsoftware.nl>
2016-08-15 09:36:09 +0200

    Fixed bugs in 4pane

--- layout/4pane.lua
+++ layout/4pane.lua
@@ -1,6 +1,8 @@
 return function(lc)
   return {
     build = function (options)
+      local toUpdate = {options.back}
+      
       local container = lc:build("stack", {width = options.width, height = options.height})
       container:addChild(options.back)
       
@@ -15,6 +17,7 @@ return function(lc)
       else
         topleft = lc:build("filler", { right = "fill", bottom = "fill" })
         topleft:addChild(options.topleft)
+        table.insert(toUpdate, options.topleft)
       end
       local bottomleft
       if not options.bottomleft then
@@ -22,6 +25,7 @@ return function(lc)
       else
         bottomleft = lc:build("filler", {right = "fill", top = "fill"})
         bottomleft:addChild(options.bottomleft)
+        table.insert(toUpdate, options.bottomleft)
       end
       
       column1:addChild(topleft)
@@ -33,6 +37,7 @@ return function(lc)
       else
         topright = lc:build("filler", {left = "fill", bottom = "fill", background = { 0, 0, 255, 255 }})
         topright:addChild(options.topright)
+        table.insert(toUpdate, options.topright)
       end
       local bottomright
       if not options.bottomright then
@@ -40,6 +45,7 @@ return function(lc)
       else
         bottomright = lc:build("filler", {left = "fill", top = "fill" })
         bottomright:addChild(options.bottomright)
+        table.insert(toUpdate, options.bottomright)
       end
       
       column2:addChild(topright)
@@ -48,6 +54,12 @@ return function(lc)
       pane:addChild( column1 )
       pane:addChild( column2 )
       container:addChild(pane)
+      
+      container.update = function(self, dt)
+        for _, view in ipairs(toUpdate) do
+          view:update(dt)
+        end
+      end
       
       return container
     end,
